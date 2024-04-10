637455a6ce073cb626b1d888be81fbbe98e1ddba
sgerda <135041922+sgerda@users.noreply.github.com>
2024-04-10 15:30:27 -0400

    main branch up to date with the test branch

--- PP4_WebSite/src/App.jsx
+++ PP4_WebSite/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
+    //now I need to do the rest of the genre and what not. use props for this
     <>
       <h1>Populars</h1>
       <MovieFetch/>
